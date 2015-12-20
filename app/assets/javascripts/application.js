71b22dbcfd8d3686897764539113b6875fa53a90
allencwwong <allencwwong@gmail.com>
2015-12-20 15:22:22 -0800

    removed theme files and added all css and js file into asset folder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,35 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
-//= require jquery_ujs
-//= require theme/bootstrap.min
-//= require theme/jquery.themepunch.tools.min
-//= require theme/jquery.themepunch.revolution.min
-//= require theme/royal_preloader.min
-//= require theme/jquery.easing.min
-//= require theme/jquery.mixitup.min
-//= require theme/jquery.magnific-popup.min
-//= require theme/jquery.snippet.min
-//= require theme/jquery.fitvids
-//= require theme/hover-dropdown
-//= require turbolinks
-//= require theme/activate-snippet
-//= require theme/bootstrap-dropdownhover.min
-//= require theme/buttons
-//= require theme/skrollr.min
-//= require theme/main
-//= require theme/owl.carousel.min
-//= require theme/scrollReveal
-//= require theme/style-switcher
-//= require theme/revolution.extension.video.min
-//= require theme/revolution.extension.actions.min
-//= require theme/revolution.extension.slideanims.min
-//= require theme/revolution.extension.carousel.min
-//= require theme/revolution.extension.kenburn.min
-//= require theme/revolution.extension.layeranimation.min
-//= require theme/revolution.extension.migration.min
-//= require theme/revolution.extension.navigation.min
-//= require theme/revolution.extension.parallax.min
-//= require theme/main
 //= require_tree .
